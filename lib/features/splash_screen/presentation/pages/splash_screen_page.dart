1b3878c347dc8276940e39989548d6e9efdff92d
NaufalAjaSih <108167231+NaufalAjaSih@users.noreply.github.com>
2026-02-02 04:18:18 +0700

    feat: Integrate `sqlite3_flutter_libs` plugin and add initial splash screen assets.

--- lib/features/splash_screen/presentation/pages/splash_screen_page.dart
+++ lib/features/splash_screen/presentation/pages/splash_screen_page.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
 
-  static const String routeName = '/splash_screen';
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _fadeAnimation;
+  late final Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..forward();
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.25),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('SplashScreen')),
-      body: const Center(child: Text('SplashScreen Page')),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/splash-background.png', fit: BoxFit.cover),
+
+          Container(color: Colors.white.withOpacity(0.3)),
+
+          Center(
+            child: SlideTransition(
+              position: _slideAnimation,
+              child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: Image.asset('assets/images/logo.png', width: 100),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
